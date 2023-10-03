4af3f4be4f7e1f971818e3947ba8e24f14c9668c
vinicius guterres <viniguter@gmail.com>
2023-10-03 01:04:39 -0300

    Init AudioWaveForm component

---
+++ src/components/AudioWaveForm.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const AudioWaveForm:FC = () => {
+    return (
+        <div className="App">
+            AudioWaveForm
+        </div>
+    );
+}
+
+export default AudioWaveForm;
